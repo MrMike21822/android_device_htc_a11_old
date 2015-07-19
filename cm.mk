e84eebd186def8d531f2bf7d2fecde7527563f04
Greg Willard <wickett06@gmail.com>
2015-07-19 12:37:48 -0400

    a11: Allow CM to lunch

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/telephony.mk)
 PRODUCT_RELEASE_NAME := a11
 
 # Inherit device configuration
-$(call inherit-product, device/htc/a11/device_a11.mk)
+$(call inherit-product, device/htc/a11/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a11
