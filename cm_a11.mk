f78fade303d119442b0286d59371fa11a94628a6
Greg Willard <wickett06@gmail.com>
2015-07-19 12:34:14 -0400

    a11: Whoops My bad

--- cm_a11.mk
+++ cm_a11.mk
@@ -1,18 +1,18 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# CM Common
+$(call inherit-product, vendor/cm/config/common.mk)
+
+# CM Telephony
+$(call inherit-product, vendor/cm/config/telephony.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := a11
-
-# Omni Common
-$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/a11/device_a11.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a11
-PRODUCT_NAME := omni_a11
+PRODUCT_NAME := cm_a11
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Desire 510
 PRODUCT_MANUFACTURER := htc
